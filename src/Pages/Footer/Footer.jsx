46ebb7a5e9c2d3c2fda247e0d182b7ea6c2d32d7
ibrohimjon1209 <ibrohimjonmamirov120909@gmail.com>
2026-03-27 22:07:48 +0500

    sdf

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import logo from './Images/logo.svg'
+import instagram from './Images/instagram.svg'
+import facebook from './Images/facebook.svg'
+import youtube from './Images/youtube.svg'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='w-full h-[642px] bg-[#1B1B1B] flex flex-col items-center'>
+
+      <img src={logo} className='w-[126px] h-[29px] mt-[60px]' />
+
+      <div className="regions">
+        <div className='w-[787px] bg-gray-300 flex justify-evenly'>
+          <h1 className='font-inter font-[500] text-[16px] text-white'>Buxoro</h1>
+          <h1 className='font-inter font-[500] text-[16px] text-white'>Samarqand</h1>
+          <h1 className='font-inter font-[500] text-[16px] text-white'>Xiva</h1>
+          <h1 className='font-inter font-[500] text-[16px] text-white'>Urganch</h1>
+          <h1 className='font-inter font-[500] text-[16px] text-white'>Nukus</h1>
+          <h1 className='font-inter font-[500] text-[16px] text-white'>Termiz</h1>
+          <h1 className='font-inter font-[500] text-[16px] text-white'>Shahrisabz</h1>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
